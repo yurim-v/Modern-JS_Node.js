bf1a8f4981e5ebcf4f7f4c814637537826a9507c
yurim-v <urdh96.w@gmail.com>
2024-06-23 16:45:43 +0900

    for ~ in 반복문_2024.06.23

---
+++ Chapter.7~8/for_in.js
@@ -0,0 +1,16 @@
+// 2024.06.23
+
+// for ~ in 반복문
+
+let group = {
+  mem1 : '민수',
+  mem2 : '영희',
+  mem3 : '유진',
+  mem4 : '철수'
+}
+
+for(let mem in group){
+  console.log(mem);
+  console.log(group[mem]);
+}
+
